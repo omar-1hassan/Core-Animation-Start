b755b7169703bcfece6306edf7bcc58a05b1168a
om-del <https://www.github.com/om-del>
2023-06-16 16:48:55 +0300

    First commit

--- Core Animation Start/ViewController.swift
+++ Core Animation Start/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alertView: UIView!
+    @IBOutlet weak var alertViewTopConstraint: NSLayoutConstraint!
+    var isActive = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hideAlert()
+
     }
 
-
+    @IBAction func btnClicked(_ sender: Any) {
+        animation(show: !isActive)
+    }
+    private func animation(show: Bool) {
+        if show {
+            showAlert()
+        } else{
+            hideAlert()
+        }
+    }
+    private func hideAlert(){
+        isActive = false
+        alertViewTopConstraint.constant = -(alertView.frame.origin.y) - alertView.frame.height
+        
+    }
+    private func showAlert(){
+        isActive = true
+        alertViewTopConstraint.constant = 20
+    }
+    
 }
 
